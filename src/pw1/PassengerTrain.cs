441a19115180a9030663119cf448175391ea295a
Fontecha111 <9404133@alumnos.ufv.es>
2025-06-09 11:59:31 +0200

    Created PassengerTrain class

---
+++ src/pw1/PassengerTrain.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace practicalwork
+{
+    public class PassengerTrain : Train
+    {
+        private int numberOfCarriages;
+        private int capacity;
+
+        public PassengerTrain(string ID, int arrivalTime, string type, int numberOfCarriages, int capacity) : base(ID, arrivalTime, type)
+        {
+            this.numberOfCarriages = numberOfCarriages;
+            this.capacity = capacity;
+        }
+
+        public int getNumberOfCarriages()
+        {
+            return this.numberOfCarriages;
+        }
+
+        public int getCapacity()
+        {
+            return this.capacity;
+        }
+    }
+}
