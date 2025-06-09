b59299c739880664c0f624360039b9a26f71e7f9
Fontecha111 <9404133@alumnos.ufv.es>
2025-06-09 15:57:56 +0200

    Created Platform

---
+++ src/pw1/Platform.cs
@@ -0,0 +1,87 @@
+using System;
+
+namespace practicalwork
+{
+    public enum PlatformStatus
+    {
+        Free,
+        Occupied
+    }
+
+    public class Platform
+    {
+        private string ID;
+        private PlatformStatus status;
+        private Train currentTrain;
+        private int dockingTime;
+
+        public Platform(string ID)
+        {
+            this.ID = ID;
+            this.status = PlatformStatus.Free;
+            this.currentTrain = null;
+            this.dockingTime = 0;
+        }
+
+        public string GetID()
+        {
+            return this.ID;
+        }
+
+        public PlatformStatus GetStatus()
+        {
+            return this.status;
+        }
+
+        public Train GetCurrentTrain()
+        {
+            return this.currentTrain;
+        }
+
+        public int GetDockingTime()
+        {
+            return this.dockingTime;
+        }
+
+        public void AssignTrain(Train train)
+        {
+            this.currentTrain = train;
+            this.status = PlatformStatus.Occupied;
+            this.dockingTime = 2;
+            train.SetStatus(Status.Docking);
+        }
+
+        public void AdvanceTick()
+        {
+            if (status == PlatformStatus.Occupied && dockingTime > 0)
+            {
+                dockingTime--;
+                if (dockingTime == 0)
+                {
+                    currentTrain.SetStatus(Status.Docked);
+                }
+            }
+        }
+
+        public void FreePlatform()
+        {
+            if (dockingTime == 0 && currentTrain != null && currentTrain.getStatus() == Status.Docked)
+            {
+                currentTrain = null;
+                status = PlatformStatus.Free;
+            }
+        }
+
+        public string ShowInfo()
+        {
+            if (status == PlatformStatus.Free)
+            {
+                return $"Platform {ID}: Free";
+            }
+            else
+            {
+                return $"Platform {ID}: Occupied by the Train {currentTrain.getID()} with status: {currentTrain.getStatus()} - Remaining Ticks: {dockingTime}";
+            }
+        }
+    }
+}
